bde1d6b11b1f4bc873a3da9f52cd930c0ccef5cf
Yiorgis Gozadinos <ggozad@crypho.com>
2020-01-16 14:02:01 +0100

    Android implementation

--- android/src/main/java/com/reactlibrary/SecureRandomModule.java
+++ android/src/main/java/com/reactlibrary/SecureRandomModule.java
@@ -1,17 +1,24 @@
 package com.crypho.securerandom;
 
+import java.security.SecureRandom;
+import android.util.Base64;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+
 
 public class SecureRandomModule extends ReactContextBaseJavaModule {
+
+    SecureRandom prng;
 
     private final ReactApplicationContext reactContext;
 
     public SecureRandomModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        prng = new SecureRandom();
     }
 
     @Override
@@ -20,8 +27,9 @@ public class SecureRandomModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void randomBytesAsync(int length, final Promise promise) {
+        byte[] output = new byte[length];
+        prng.nextBytes(output);
+        promise.resolve(Base64.encodeToString(output, Base64.NO_WRAP));
     }
 }
