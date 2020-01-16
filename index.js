4773149daa310ae210a894a4548318bc3065e884
Wichert Akkerman <wichert@wiggy.net>
2020-01-16 14:21:57 +0100

    Coding style cleanups

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 import { NativeModules } from 'react-native'
-const Buffer = require('buffer').Buffer
+import { Buffer } from 'buffer'
+
 const { SecureRandom } = NativeModules
 
 const POOL_SIZE = 1024
-let pool = []
+const pool = []
 
-const b64ToUInt8 = function(b64) {
+function b64ToUInt8(b64) {
   return new Uint8Array(Buffer(b64, 'base64'))
 }
 
-const fillPool = function() {
+function fillPool() {
   if (pool.length < POOL_SIZE) {
     randomBytesAsync(POOL_SIZE - pool.length).then(seed => {
-      pool.splice(pool.length, 0, ...seed)
+      pool.push(...seed)
     })
   }
 }
 
-const randomBytesAsync = function(length) {
+function randomBytesAsync(length) {
   return SecureRandom.randomBytesAsync(length).then(b64ToUInt8)
 }
 
-const randomBytesSync = function(length) {
+function randomBytesSync(length) {
   if (length > pool.length) {
-    throw 'Random pool depleted'
+    throw new Error('Random pool depleted')
   }
   const bytes = pool.splice(0, length)
   fillPool()
