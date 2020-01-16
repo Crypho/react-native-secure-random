720935343e1d0a91eb516ddebaecfa829e80295d
Yiorgis Gozadinos <ggozad@crypho.com>
2020-01-16 10:30:28 +0100

    randomBytesAsync ios

--- ios/SecureRandom.m
+++ ios/SecureRandom.m
@@ -5,10 +5,18 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_REMAP_METHOD(randomBytesAsync,
+                 randomBytesAsync:(NSUInteger)length
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    NSMutableData *bytes = [NSMutableData dataWithLength:length];
+    OSStatus result = SecRandomCopyBytes(kSecRandomDefault, length, [bytes mutableBytes]);
+    if (result == errSecSuccess) {
+        resolve([bytes base64EncodedStringWithOptions:0]);
+    } else {
+        reject(@"ERR_RANDOM", @"Failed to create a secure random bytes", [NSError errorWithDomain:@"react-native-secure-random" code:result userInfo:nil]);
+    }
 }
 
 @end
