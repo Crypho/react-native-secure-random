226f744491e9d8327a912596b9a3d5ebab1857f4
Yiorgis Gozadinos <ggozad@crypho.com>
2020-01-16 13:31:37 +0100

    Sync version of randomBytes

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from 'react-native'
+const Buffer = require('buffer').Buffer
+const { SecureRandom } = NativeModules
 
-const { SecureRandom } = NativeModules;
+const POOL_SIZE = 1024
+let pool = []
 
-export default SecureRandom;
+const b64ToUInt8 = function(b64) {
+  return new Uint8Array(Buffer(b64, 'base64'))
+}
+
+const fillPool = function() {
+  if (pool.length < POOL_SIZE) {
+    randomBytesAsync(POOL_SIZE - pool.length).then(seed => {
+      pool.splice(pool.length, 0, ...seed)
+    })
+  }
+}
+
+const randomBytesAsync = function(length) {
+  return SecureRandom.randomBytesAsync(length).then(b64ToUInt8)
+}
+
+const randomBytesSync = function(length) {
+  if (length > pool.length) {
+    throw 'Random pool depleted'
+  }
+  const bytes = pool.splice(0, length)
+  fillPool()
+  return bytes
+}
+
+fillPool()
+
+export { randomBytesAsync, randomBytesSync }
